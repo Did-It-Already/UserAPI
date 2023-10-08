c9e71545c700da803f2d8b4e20d180cdb4de40f8
npardom <npardom@unal.edu.co>
2023-10-07 19:24:02 -0500

    model changed

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     user_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.CharField(max_length=200, unique=True)
     profile_picture = models.TextField(default='', blank=True)
     theme = models.CharField(max_length=20, default='light')
 
